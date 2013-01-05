4499c6dce169edcac5cfa96bdd10d1d02fdab65c
heppu <ville.rasanen@aalto.fi>
2013-01-05 14:42:07 +0200

    + IllegalOperationException.java

---
+++ Titanium-UI/src/org/icefaces/samples/showcase/example/ace/dataTable/IllegalOperationException.java
@@ -0,0 +1,9 @@
+package org.icefaces.samples.showcase.example.ace.dataTable;
+
+public class IllegalOperationException extends Exception {
+	
+	public IllegalOperationException(String s){
+		super(s);
+	}
+
+}
