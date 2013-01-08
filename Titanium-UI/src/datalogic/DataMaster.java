33a052f6249704ca3124fb74c5e80a9adfcc4e2e
heppu <ville.rasanen@aalto.fi>
2013-01-08 16:17:05 +0200

    Schedulings viewin logiikka refaktoroitu SchedulingDataManager luokkaan. UI:n siistimistä editissä ja addissä.

--- Titanium-UI/src/datalogic/DataMaster.java
+++ Titanium-UI/src/datalogic/DataMaster.java
@@ -1,11 +1,7 @@
 package datalogic;
 
 import java.io.Serializable;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 
@@ -16,15 +12,12 @@ import javax.faces.bean.ManagedProperty;
 
 import org.icefaces.ace.component.datatable.DataTable;
 import org.icefaces.ace.event.ExpansionChangeEvent;
-import org.icefaces.ace.model.table.RowState;
 import org.icefaces.ace.model.table.RowStateMap;
 
 import entities.Backend;
-import entities.Comment;
 import entities.Composite;
 import entities.Instance;
 import entities.Mode;
-import entities.Scheduling;
 
 @ManagedBean
 @CustomScoped(value = "#{window}")
@@ -39,17 +32,9 @@ public class DataMaster implements Serializable {
 
 	private DataTable i_dataTable;
 
-	private List<Scheduling> schedulings = new ArrayList<Scheduling>();
-
 	private List<Instance> instances;
 
-	private RowStateMap stateMap = new RowStateMap();
-
 	private RowStateMap i_stateMap = new RowStateMap();
-
-	private HashMap<Integer, SchedulingBuilder> editBuffer = new HashMap<Integer, SchedulingBuilder>();
-	private HashMap<Integer, List<Comment>> commentList = new HashMap<Integer, List<Comment>>();
-	private HashMap<Integer, Comment> addCommentList = new HashMap<Integer, Comment>();
 
 	private HttpConnector http = new HttpConnector();
 
@@ -59,7 +44,6 @@ public class DataMaster implements Serializable {
 	private Collection<Composite> composites = SessionBean.COMPOSITES.values();
 	private Collection<Backend> backends = SessionBean.BACKENDS.values();
 	
-	private Comment comment = new Comment();
 
 	private boolean editError;
 	private String editErrorMessage;
@@ -73,81 +57,7 @@ public class DataMaster implements Serializable {
 		this.builder = new SchedulingBuilder();
 	}
 
-	public void addScheduling() {
-
-		Scheduling s;
-		try {
-			s = this.builder.build();
-			session.getConnector().addScheduling(s);
-			if(comment.getText() != null  && !comment.getText().equals("")){
-				DateFormat format = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
-				comment.setSchedulingID(s.getId());
-				System.out.println(comment.getSchedulingID());
-				System.out.println(format.format(new Date()));
-				System.out.println(comment.getText());
-				comment.setCreationDate(format.format(new Date()).toString());
-				session.getConnector().addComment(comment);
-				
-			}
-			
-			System.out.println(s.getId());
-			addError = false;
-			System.out.println("HttpConnector returned: " + http.addId(SessionBean.COMPOSITES.get(s.getServiceID()).getDestinationURL(), s.getId()));
-		} catch (IllegalOperationException e) {
-			addErrorMessage = e.getMessage();
-			addError = true;
-		}
-
-	}
-
-	public void confirmEdit(Scheduling s) {
-
-		try {
-
-			Scheduling n = this.editBuffer.get(s.getId()).build();
-			this.schedulings.remove(s);
-			this.schedulings.add(n);
-
-			//this.session.getConnector().updateScheduling(n);
-			editError = false;
-			System.out.println("HttpConnector returned: " + http.editId(SessionBean.COMPOSITES.get(s.getServiceID()).getDestinationURL(), s.getId()));
-		} catch (IllegalOperationException e) {
-			editErrorMessage = e.getMessage();
-			editError = true;
-		}
-
-	}
-
-	public void expansion(ExpansionChangeEvent e) {
-		Scheduling s = (Scheduling) e.getRowData();
-		if (e.isExpanded()) {
-			Comment c = new Comment();
-			c.setText("ARGH");
-			c.setSchedulingID(s.getId());
-			this.addCommentList.put(s.getId(), c);
-			this.commentList.put(s.getId(), this.session.getConnector()
-					.getLastComments(s.getId()));
-			this.editBuffer.put(s.getId(), new SchedulingBuilder(s));
-			this.editError = false;
-		} else {
-			this.commentList.remove(s.getId());
-			this.editBuffer.remove(s.getId());
-		}
-	}
-	
-	public void submitComment(int id){
-		
-		Comment c = this.addCommentList.get(id);
-		DateFormat format = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
-		System.out.println(format.format(new Date()));
-		System.out.println(c.getText());
-		c.setCreationDate(format.format(new Date()).toString());
-		this.session.getConnector().addComment(c);
-		this.commentList.get(id).add(c);
-	}
-
 	public void instanceExpansion(ExpansionChangeEvent e){
-
 		int Id = ((Instance) e.getRowData()).getId();
 		if (e.isExpanded()){
 			this.instanceEditBuffer.put(Id, (Instance) e.getRowData());
@@ -157,65 +67,11 @@ public class DataMaster implements Serializable {
 		}
 		
 	}
-	public void resetEdit(Scheduling s) {
-		SchedulingBuilder b = new SchedulingBuilder(s);
-
-		this.editBuffer.put(s.getId(), new SchedulingBuilder(s));
-	}
-
-	public void selectAll() {
-		List<Scheduling> filteredRows = dataTable.getFilteredData();
-		if (filteredRows == null || filteredRows.isEmpty()) {
-			stateMap.setAllSelected(true);
-			return;
-		}
-		for (Scheduling s : filteredRows) {
-			stateMap.get(s).setSelected(true);
-		}
-	}
-
-	public void deselectAll() {
-		Collection<RowState> allRows = stateMap.values();
-
-		for (RowState s : allRows) {
-			s.setSelected(false);
-		}
-	}
-
-	public void startSelected() {
-		for (Object rowData : stateMap.getSelected()) {
-			Scheduling s = (Scheduling) rowData;
-
-			s.setStatusID(SessionBean.ENABLED);
-
-			//this.session.getConnector().updateScheduling(s);
-			System.out.println("HttpConnector returned: " + http.editId(SessionBean.COMPOSITES.get(s.getServiceID()).getDestinationURL(), s.getId()));
-		}
-	}
-
-	public void stopSelected() {
-
-		for (Object rowData : stateMap.getSelected()) {
-			Scheduling s = (Scheduling) rowData;
-
-			s.setStatusID(SessionBean.DISABLED);
-
-			//this.session.getConnector().updateScheduling(s);
-			System.out.println("HttpConnector returned: " + http.editId(SessionBean.COMPOSITES.get(s.getServiceID()).getDestinationURL(), s.getId()));
-		}
-	}
-	
-	public void runSelectedSchedules() {
-		for (Object rowData : stateMap.getSelected()) {
-			Scheduling s = (Scheduling) rowData;
-			System.out.println("HttpConnector returned: " + http.runId(SessionBean.COMPOSITES.get(s.getServiceID()).getDestinationURL(), s.getId()));
-		}
-	}
 
 	public void runInstances() {
-
 		System.out.println("run instances");
 	}
+	
 	public DataTable getDataTable() {
 		return dataTable;
 	}
@@ -240,12 +96,7 @@ public class DataMaster implements Serializable {
 		this.session = session;
 	}
 
-	public List<Scheduling> getSchedulings() {
-		if (this.schedulings == null || this.schedulings.isEmpty()) {
-			this.refresh();
-		}
-		return schedulings;
-	}
+
 
 	public List<Instance> getInstances() {
 		if (this.instances == null || this.instances.isEmpty()) {
@@ -262,22 +113,7 @@ public class DataMaster implements Serializable {
 		this.instances = instances;
 	}
 
-	public void refresh() {
-		this.schedulings.clear();
-		this.schedulings = this.session.getConnector().getSchedulings();
-	}
-
-	public void setSchedulings(List<Scheduling> schedulings) {
-		this.schedulings = schedulings;
-	}
-
-	public RowStateMap getStateMap() {
-		return stateMap;
-	}
-
-	public void setStateMap(RowStateMap stateMap) {
-		this.stateMap = stateMap;
-	}
+	
 
 	public RowStateMap getI_stateMap() {
 		return i_stateMap;
@@ -285,14 +121,6 @@ public class DataMaster implements Serializable {
 
 	public void setI_stateMap(RowStateMap i_stateMap) {
 		this.i_stateMap = i_stateMap;
-	}
-
-	public HashMap<Integer, SchedulingBuilder> getEditBuffer() {
-		return editBuffer;
-	}
-
-	public void setEditBuffer(HashMap<Integer, SchedulingBuilder> editBuffer) {
-		this.editBuffer = editBuffer;
 	}
 
 	public HashMap<Integer, Instance> getInstanceEditBuffer() {
@@ -366,29 +194,4 @@ public class DataMaster implements Serializable {
 	public void setAddErrorMessage(String addErrorMessage) {
 		this.addErrorMessage = addErrorMessage;
 	}
-
-	public HashMap<Integer, List<Comment>> getCommentList() {
-		return commentList;
-	}
-
-	public void setCommentList(HashMap<Integer, List<Comment>> commentList) {
-		this.commentList = commentList;
-	}
-
-	public HashMap<Integer, Comment> getAddCommentList() {
-		return addCommentList;
-	}
-
-	public void setAddCommentList(HashMap<Integer, Comment> addCommentList) {
-		this.addCommentList = addCommentList;
-	}
-
-	public Comment getComment() {
-		return comment;
-	}
-
-	public void setComment(Comment comment) {
-		this.comment = comment;
-	}
-
 }
---
+++ Titanium-UI/src/datalogic/SchedulingDataManager.java
@@ -0,0 +1,452 @@
+package datalogic;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.SessionScoped;
+
+import org.icefaces.ace.component.datatable.DataTable;
+import org.icefaces.ace.event.ExpansionChangeEvent;
+import org.icefaces.ace.model.table.RowState;
+import org.icefaces.ace.model.table.RowStateMap;
+
+import entities.Backend;
+import entities.Comment;
+import entities.Composite;
+import entities.Mode;
+import entities.Scheduling;
+
+@ManagedBean(name = "schedulingDataManager")
+@SessionScoped
+public class SchedulingDataManager {
+	
+	@ManagedProperty(value = "#{sessionBean}")
+	private SessionBean session;
+
+	private SchedulingBuilder builder;
+	private Comment addComment = new Comment();
+
+	private List<Scheduling> schedulings = new ArrayList<Scheduling>();
+	private RowStateMap stateMap = new RowStateMap();
+	private DataTable dataTable;
+
+	private HashMap<Integer, SchedulingBuilder> editBuffer = new HashMap<Integer, SchedulingBuilder>();
+	private HashMap<Integer, List<Comment>> commentLists = new HashMap<Integer, List<Comment>>();
+	private HashMap<Integer, Comment> editCommentList = new HashMap<Integer, Comment>();
+	
+	private Collection<Mode> modes = SessionBean.MODES.values();
+	private Collection<Composite> composites = SessionBean.COMPOSITES.values();
+	private Collection<Backend> backends = SessionBean.BACKENDS.values();
+	
+	private boolean showEditError;
+	private String editErrorMessage;
+
+	private boolean showAddError;
+	private String addErrorMessage;
+
+	private static final DateFormat DATE_FORMAT = new SimpleDateFormat(
+			"dd-MM-yyyy HH:mm:ss");
+
+	private HttpConnector httpConnector = new HttpConnector();
+
+	@PostConstruct
+	private void init() {
+		this.builder = new SchedulingBuilder();
+	}
+
+	/**
+	 * Method to be called from the UI when a new {@link Scheduling} is to be
+	 * constructed from the {@link SchedulingBuilder}
+	 */
+	public void addScheduling() {
+
+		try {
+
+			/*
+			 * Build the Scheduling from values inserted to the builder. Values
+			 * are validated within the builder and an IllegalOperationException
+			 * will be thrown if the values are invalid
+			 */
+			Scheduling s = this.builder.build();
+
+			this.session.getConnector().addScheduling(s);
+
+			/*
+			 * If there is some text in addComment, we'll add it straight away.
+			 * It will be connected by the ID of previously added Scheduling so
+			 * its important to add the Comment after the Scheduling has been
+			 * persisted and an ID has been assigned to it.
+			 */
+			if (addComment.getText() != null
+					&& !"".equals(addComment.getText())) {
+				addComment.setSchedulingID(s.getId());
+				addComment.setCreationDate(DATE_FORMAT.format(new Date()));
+				session.getConnector().addComment(addComment);
+			}
+
+			// If we've gotten this far, there were no errors and we can hide
+			// all error messages
+			showAddError = false;
+
+			// Http request, consult Hanzki for any details
+			System.out.println("HttpConnector returned: "
+					+ httpConnector.addId(
+							SessionBean.COMPOSITES.get(s.getServiceID())
+									.getDestinationURL(), s.getId()));
+		} catch (IllegalOperationException e) {
+
+			/*
+			 * An error was thrown during the validation of the Scheduling: we
+			 * set the error message to be shown in Add Scheduling section and
+			 * set the visibility to true
+			 */
+			addErrorMessage = e.getMessage();
+			showAddError = true;
+		}
+	}
+
+	/**
+	 * Method to be called from the UI when a row is expanded or closed to
+	 * initialize or clear the needed buffers for {@link Comment} and
+	 * {@link SchedulingBuilder} instances. The {@link ExpansionChangeEvent}
+	 * passed as a parameter contains the Scheduling corresponding to the row.
+	 */
+	public void expansion(ExpansionChangeEvent e) {
+
+		// Retrieve the Scheduling corresponding to the row
+		Scheduling s = (Scheduling) e.getRowData();
+
+		if (e.isExpanded()) {
+
+			/*
+			 * Initialize the editing for this row by creating a new
+			 * SchedulingBuilder and adding it to the editBuffer with the ID of
+			 * the corresponding Scheduling as its key
+			 */
+			this.editBuffer.put(s.getId(), new SchedulingBuilder(s));
+
+			/*
+			 * Retrieve the last five Comments of this Scheduling from the
+			 * database and add them to the HashMap of lists with the ID of the
+			 * Scheduling as key
+			 */
+			this.commentLists.put(s.getId(), this.session.getConnector()
+					.getLastComments(s.getId()));
+
+			/*
+			 * Create a new empty comment, set its SchedulingID to match this
+			 * Scheduling and add it to the comment buffer
+			 */
+			Comment c = new Comment();
+			c.setSchedulingID(s.getId());
+			this.editCommentList.put(s.getId(), c);
+
+			/*
+			 * Since we just expanded a new row, we want to clear all error
+			 * messages that would be shown in it
+			 */
+			this.showEditError = false;
+		} else {
+			/*
+			 * When the row is closed, we remove the SchedulingBuilder from the
+			 * editBuffer so it doesn't keep the changes that were made. We also
+			 * clear the list of comments assigned to this row so it will be
+			 * refreshed when the row is expanded again
+			 */
+			this.commentLists.remove(s.getId());
+			this.editBuffer.remove(s.getId());
+		}
+	}
+
+	/**
+	 * Method to be called from the UI when the edit of a {@link Scheduling} is
+	 * to be confirmed
+	 */
+	public void confirmEdit(Scheduling s) {
+
+		try {
+
+			/*
+			 * Retrieve the SchedulingBuilder corresponding to the id of
+			 * Scheduling passed as a parameter from the UI and build it.
+			 * Validation is carried out normally within the builder
+			 */
+			Scheduling n = this.editBuffer.get(s.getId()).build();
+
+			/*
+			 * In order to refresh the DataTable we need to "brute force" the
+			 * change by removing the unedited Scheduling and adding the newly
+			 * built one
+			 */
+			this.schedulings.remove(s);
+			this.schedulings.add(n);
+
+			// this.session.getConnector().updateScheduling(n);
+
+			// If we've gotten this far there were no errors and we can hide all
+			// error messages
+			showEditError = false;
+
+			// Http request, consult Hanzki for any details
+			System.out.println("HttpConnector returned: "
+					+ httpConnector.editId(
+							SessionBean.COMPOSITES.get(s.getServiceID())
+									.getDestinationURL(), s.getId()));
+		} catch (IllegalOperationException e) {
+
+			/*
+			 * An error was thrown during the validation of the edited
+			 * Scheduling: we set the error message to be shown in Edit section
+			 * and set the visibility to true
+			 */
+			editErrorMessage = e.getMessage();
+			showEditError = true;
+		}
+
+	}
+
+	/**
+	 * Method to be called from the UI when the fields in the Edit view are to
+	 * be reset to their initial state
+	 */
+	public void resetEdit(Scheduling s) {
+		/*
+		 * Reset the fields in the Edit view by replacing the SchedulingBuilder
+		 * by a new one initialized with the Scheduling passed as a parameter
+		 * from UI
+		 */
+		SchedulingBuilder b = new SchedulingBuilder(s);
+
+		this.editBuffer.put(s.getId(), b);
+	}
+
+	/**
+	 * Method to be called from the UI when the comment in the Edit view is to
+	 * be submitted to the database
+	 */
+	public void submitComment(int id) {
+		Comment c = this.editCommentList.get(id);
+		c.setCreationDate(DATE_FORMAT.format(new Date()));
+		this.session.getConnector().addComment(c);
+		this.commentLists.get(id).add(c);
+	}
+
+	/**
+	 * Method to be called from the UI when Select All button is pressed and all
+	 * rows currently filtered (or lazily loaded) are to be selected. The [@link
+	 * DataTable} is bound to this bean so we can operate on the filtered data.
+	 */
+	public void selectAll() {
+
+		List<Scheduling> filteredRows = dataTable.getFilteredData();
+
+		/*
+		 * If the filtered data is empty or null, we do not currently have a
+		 * filter. To prevent a NullPointerException we use the standard method
+		 * of the stateMap for selecting all rows
+		 */
+		if (filteredRows == null || filteredRows.isEmpty()) {
+			stateMap.setAllSelected(true);
+			return;
+		}
+		
+		// Iterate through the filtered data and set all rows to be selected
+		for (Scheduling s : filteredRows) {
+			stateMap.get(s).setSelected(true);
+		}
+	}
+	
+	/**
+	 * Method to be called from the UI when Deselect All button is pressed and all
+	 * selected rows are to be deselected.
+	 */
+	public void deselectAll() {
+		Collection<RowState> allRows = stateMap.values();
+
+		for (RowState s : allRows) {
+			s.setSelected(false);
+		}
+	}
+	
+	
+	//TODO Comments
+	public void startSelected() {
+		for (Object rowData : stateMap.getSelected()) {
+			Scheduling s = (Scheduling) rowData;
+
+			s.setStatusID(SessionBean.ENABLED);
+
+			//this.session.getConnector().updateScheduling(s);
+			System.out.println("HttpConnector returned: " + httpConnector.editId(SessionBean.COMPOSITES.get(s.getServiceID()).getDestinationURL(), s.getId()));
+		}
+	}
+	
+	//TODO Comments
+	public void stopSelected() {
+
+		for (Object rowData : stateMap.getSelected()) {
+			Scheduling s = (Scheduling) rowData;
+
+			s.setStatusID(SessionBean.DISABLED);
+
+			//this.session.getConnector().updateScheduling(s);
+			System.out.println("HttpConnector returned: " + httpConnector.editId(SessionBean.COMPOSITES.get(s.getServiceID()).getDestinationURL(), s.getId()));
+		}
+	}
+	
+	//TODO Comments
+	public void runSelectedSchedules() {
+		for (Object rowData : stateMap.getSelected()) {
+			Scheduling s = (Scheduling) rowData;
+			System.out.println("HttpConnector returned: " + httpConnector.runId(SessionBean.COMPOSITES.get(s.getServiceID()).getDestinationURL(), s.getId()));
+		}
+	}
+	
+	/**
+	 * Method for refreshing the contents of the data table.
+	 */
+	public void refresh() {
+		this.schedulings.clear();
+		this.schedulings = this.session.getConnector().getSchedulings();
+	}
+	
+	public SessionBean getSession() {
+		return session;
+	}
+
+	public void setSession(SessionBean session) {
+		this.session = session;
+	}
+
+	public SchedulingBuilder getBuilder() {
+		return builder;
+	}
+
+	public void setBuilder(SchedulingBuilder builder) {
+		this.builder = builder;
+	}
+
+	public Comment getAddComment() {
+		return addComment;
+	}
+
+	public void setAddComment(Comment addComment) {
+		this.addComment = addComment;
+	}
+
+	public List<Scheduling> getSchedulings() {
+		if (this.schedulings == null || this.schedulings.isEmpty()) {
+			this.refresh();
+		}
+		return schedulings;
+	}
+
+	public void setSchedulings(List<Scheduling> schedulings) {
+		this.schedulings = schedulings;
+	}
+
+	public RowStateMap getStateMap() {
+		return stateMap;
+	}
+
+	public void setStateMap(RowStateMap stateMap) {
+		this.stateMap = stateMap;
+	}
+
+	public HashMap<Integer, SchedulingBuilder> getEditBuffer() {
+		return editBuffer;
+	}
+
+	public void setEditBuffer(HashMap<Integer, SchedulingBuilder> editBuffer) {
+		this.editBuffer = editBuffer;
+	}
+
+	public HashMap<Integer, List<Comment>> getCommentLists() {
+		return commentLists;
+	}
+
+	public void setCommentList(HashMap<Integer, List<Comment>> commentList) {
+		// Cannot be set from the UI but the setter has to exist
+	}
+
+	public HashMap<Integer, Comment> getEditCommentList() {
+		return editCommentList;
+	}
+
+	public void setEditCommentList(HashMap<Integer, Comment> editCommentList) {
+		this.editCommentList = editCommentList;
+	}
+
+	public boolean isShowEditError() {
+		return showEditError;
+	}
+
+	public void setEditError(boolean editError) {
+		// Cannot be set from the UI but the setter has to exist
+	}
+
+	public String getEditErrorMessage() {
+		return editErrorMessage;
+	}
+
+	public void setEditErrorMessage(String editErrorMessage) {
+		this.editErrorMessage = editErrorMessage;
+	}
+
+	public boolean isShowAddError() {
+		return showAddError;
+	}
+
+	public void setShowAddError(boolean addError) {
+		// Cannot be set from the UI but the setter has to exist
+	}
+
+	public String getAddErrorMessage() {
+		return addErrorMessage;
+	}
+
+	public void setAddErrorMessage(String addErrorMessage) {
+		this.addErrorMessage = addErrorMessage;
+	}
+
+	public DataTable getDataTable() {
+		return dataTable;
+	}
+
+	public void setDataTable(DataTable dataTable) {
+		this.dataTable = dataTable;
+	}
+
+	public Collection<Mode> getModes() {
+		return modes;
+	}
+
+	public void setModes(Collection<Mode> modes) {
+		this.modes = modes;
+	}
+
+	public Collection<Composite> getComposites() {
+		return composites;
+	}
+
+	public void setComposites(Collection<Composite> composites) {
+		this.composites = composites;
+	}
+
+	public Collection<Backend> getBackends() {
+		return backends;
+	}
+
+	public void setBackends(Collection<Backend> backends) {
+		this.backends = backends;
+	}
+
+}
