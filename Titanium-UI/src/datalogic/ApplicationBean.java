307146e783e3dab597172dc9beb4c55f6f8a5982
heppu <ville.rasanen@aalto.fi>
2013-01-21 22:51:02 +0200

    Jaoin ui:ta useisiin xhtml tiedostoihin selkeyden vuoksi

--- Titanium-UI/src/datalogic/ApplicationBean.java
+++ Titanium-UI/src/datalogic/ApplicationBean.java
@@ -16,17 +16,17 @@ import entities.Composite;
 import entities.Mode;
 import entities.Status;
 
-@ManagedBean(name="applicationBean", eager=true)
+@ManagedBean(name = "applicationBean", eager = true)
 @ApplicationScoped
 public class ApplicationBean implements Serializable {
-	
-	public static final HashMap<Integer, Backend> BACKENDS = new HashMap<Integer, Backend>();  
-	public static final HashMap<Integer,Mode> MODES = new HashMap<Integer, Mode>();
-	public static final HashMap<Integer,Composite> COMPOSITES = new HashMap<Integer, Composite>();
+
+	public static final HashMap<Integer, Backend> BACKENDS = new HashMap<Integer, Backend>();
+	public static final HashMap<Integer, Mode> MODES = new HashMap<Integer, Mode>();
+	public static final HashMap<Integer, Composite> COMPOSITES = new HashMap<Integer, Composite>();
 	public static final HashMap<Integer, Status> STATUSES = new HashMap<Integer, Status>();
-	
+
 	public static final HashMap<Integer, String> MODE_STYLES = new HashMap<Integer, String>();
-	
+
 	public TimeZone timezone;
 	// Hard coded styleClasses for different modes
 	{
@@ -34,42 +34,58 @@ public class ApplicationBean implements Serializable {
 		MODE_STYLES.put(2, "deactivated");
 		MODE_STYLES.put(3, "removed");
 	}
-	
+
 	public static final int ENABLED = 1;
 	public static final int DISABLED = 2;
 	public static final int REMOVED = 3;
-	
+
+	// PLACEHOLDER
+	public String html = "<object>"
+			+ "<param name=\"movie\"value=\"http://myinstants.com/media/bt/genericInstant_event.swf\" />"
+			+ "<param name=\"wmode\" value=\"transparent\">"
+			+ "<param name=\"flashVars\"	value=\"color=ff0000&sound=http://myinstants.com/media/sounds/inceptionbutton.mp3\">"
+			+ "<embed src=\"http://myinstants.com/media/bt/genericInstant_event.swf\" flashVars=\"color=ff0000&sound=http://myinstants.com/media/sounds/inceptionbutton.mp3\" wmode=\"transparent\" width=\"100\" height=\"100\" />"
+			+ "</object>";
+
+	public String getHtml() {
+		return html;
+	}
+
+	public void setHtml(String html) {
+		this.html = html;
+	}
+
 	public static final DateFormat DATE_FORMAT = new SimpleDateFormat(
 			"dd-MM-yyyy HH:mm:ss");
-	
+
 	public static final DateFormat ORACLE_DATE_FORMAT = new SimpleDateFormat(
 			"yyyy-dd-MM HH:mm:ss");
-	
-	public ApplicationBean(){
-		
+
+	public ApplicationBean() {
+
 		DatabaseConnector tempConnector = new DatabaseConnector();
-		
+
 		List<Status> tempStatuses = tempConnector.getAllStatuses();
-		for(Status s: tempStatuses ){
+		for (Status s : tempStatuses) {
 			STATUSES.put(s.getId(), s);
 		}
-		
+
 		List<Composite> tempComposites = tempConnector.getAllComposites();
-		for(Composite c: tempComposites ){
+		for (Composite c : tempComposites) {
 			COMPOSITES.put(c.getId(), c);
 		}
-		
+
 		List<Backend> tempBackends = tempConnector.getAllBackends();
-		for(Backend b: tempBackends){
+		for (Backend b : tempBackends) {
 			BACKENDS.put(b.getId(), b);
 		}
 		List<Mode> tempModes = tempConnector.getAllModes();
 		for (Mode m : tempModes) {
 			MODES.put(m.getId(), m);
 		}
-		
-		this.timezone =  Calendar.getInstance().getTimeZone();
-		
+
+		this.timezone = Calendar.getInstance().getTimeZone();
+
 		tempConnector.close();
 	}
 
@@ -80,5 +96,5 @@ public class ApplicationBean implements Serializable {
 	public void setTimezone(TimeZone timezone) {
 		this.timezone = timezone;
 	}
-	
+
 }
